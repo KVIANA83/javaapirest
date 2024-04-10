b4e913cafd845c3caf9804a1f981420df1069620
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-04-10 13:51:48 -0300

    Atualizar o AlunoController.java
    update e correção no código

--- src/main/java/com/jpa/demo/controller/AlunoController.java
+++ src/main/java/com/jpa/demo/controller/AlunoController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/aluno") // Define o endpoint da aplicação, um endereco utilizado para comunicação entre uma API e um sistema externo.
 public class AlunoController {
+
     @Autowired // Injeção de dependêcia, permite que o Spring controle as instância da classe AlunoRepository
     private AlunoRepository alunoRepository;
+    
     public AlunoController(AlunoRepository alunoRepository){
         this.alunoRepository = alunoRepository;
     }
@@ -24,5 +26,32 @@ public class AlunoController {
         Aluno alunoObj = new Aluno(alunoRecord.cpf()); // alunoObj vai receber o DTO de alunoRecord
         alunoRepository.save(alunoObj); // invoca o método save para armazenar o objeto no banco de dados
         return ResponseEntity.status(HttpStatus.CREATED).build(); // Retorna o status da resposta HTTP após a compilação
+    }
+
+    // Atualizar aluno
+    @PutMapping("/{id}") 
+    public ResponseEntity<Aluno> atualizarAluno(@PathVariable int id, @RequestBody AlunoRecord alunoRecord) {
+        Optional<Aluno> alunoOptional = alunoRepository.findById(id);
+        
+        if (alunoOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        Aluno aluno = alunoOptional.get();
+        aluno.setCpf(alunoRecord.getCpf());
+        aluno.setNome(alunoRecord.getNome());
+        aluno.setDataNascimento(alunoRecord.getDataNascimento());
+        aluno.setEmail(alunoRecord.getEmail());
+        Aluno alunoAtualizado = alunoRepository.save(aluno);
+        return ResponseEntity.ok(alunoAtualizado);
+    }
+
+    // Deletar Aluno
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirAluno(@PathVariable int id) {
+        if (!alunoRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        alunoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
