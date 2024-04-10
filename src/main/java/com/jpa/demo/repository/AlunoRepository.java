fbf2da860dcf02e21dbcad942e52dd99406a46a1
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-04-10 13:53:19 -0300

    Atualizar o AlunoRepository.java
    update e correção no código

--- src/main/java/com/jpa/demo/repository/AlunoRepository.java
+++ src/main/java/com/jpa/demo/repository/AlunoRepository.java
@@ -4,6 +4,15 @@ import com.jpa.demo.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.time.LocalDate;
+
 @Repository // Informa ao Spring que esta interface atuará como um repositório de banco de dados.
 public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
+    
+    // são métodos de consulta personalizados que permitem buscar um aluno pelo campo de data de nascimento ou pelo CPF
+    Optional<Aluno> findByDataNascimento(LocalDate dataNascimento);
+    Optional<Aluno> findByCpf(String cpf);
+    
+    // Optional indica que o resultado pode ser nulo
 }
